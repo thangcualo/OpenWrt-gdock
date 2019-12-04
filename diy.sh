51bee30ad09d3ea5985dbda1e4c47da967062d15
a736399919 <55206982+a736399919@users.noreply.github.com>
2019-12-04 18:19:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -47,3 +47,4 @@ sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/d
 #修改banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
+cp ../20191204.config .config
