76105cf8d9e33aa1b9ce63f147d9b71aeff363d9
a736399919 <55206982+a736399919@users.noreply.github.com>
2019-12-05 20:17:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -47,4 +47,4 @@ sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/d
 #修改banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
-cp ../G-DOCK/20191204.config .config
+cp ../G-DOCK/2019120519.config .config
