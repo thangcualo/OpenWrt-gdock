de7824eb5bf33c5cf631ab02d64d518078eee623
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-03-24 03:31:48 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -8,9 +8,9 @@
 #克隆源码
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
-sed -i '$a src-git extra https://github.com/Andy2244/openwrt-extra.git' feeds.conf.default
+#sed -i '$a src-git extra https://github.com/Andy2244/openwrt-extra.git' feeds.conf.default
 ./scripts/feeds update -a
-./scripts/feeds install -f -p extra -a
+#./scripts/feeds install -f -p extra -a
 ./scripts/feeds install -a
 #添加自定义插件
 git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
@@ -20,11 +20,12 @@ git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
 
 #删除自带的插件
 #rm -rf feeds/extra/luci-app-samba4
-rm -rf feeds/extra/luci-app-cifsd
-sed -i 's/cifsd-tools/smbd-server/g' package/lean/luci-app-cifsd/Makefile
-rm -rf package/lean/luci-app-koolproxyR
-rm -rf package/lean/luci-app-serverchan
-rm -rf package/lean/luci-app-unblockmusic
+#rm -rf feeds/extra/luci-app-cifsd
+#sed -i 's/cifsd-tools/smbd-server/g' package/lean/luci-app-cifsd/Makefile
+#rm -rf package/lean/luci-app-koolproxyR
+#rm -rf package/lean/luci-app-serverchan
+#rm -rf package/lean/luci-app-unblockmusic
+#改qb版本为4.2.1
 rm -rf package/lean/qBittorrent/Makefile
 rm -rf package/lean/qBittorrent/patches
 cp -f ../qb421 package/lean/qBittorrent/Makefile
@@ -33,7 +34,7 @@ cp -f ../qb421 package/lean/qBittorrent/Makefile
 sed -i 's/4.14/4.19/g' target/linux/ipq40xx/Makefile
 
 #添加自己repo的插件的软连接
-ln -s ../../luci-theme-argon1.x ./package/
+ln -s ../../luci-theme-argon1.5 ./package/
 ln -s ../../luci-app-flowoffload_ADGHome ./package/
 
 #修改lan口地址
@@ -49,7 +50,7 @@ sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #修改zzz-default-settings的配置
-sed -i 's/samba/samba4/g' package/lean/default-settings/files/zzz-default-settings	#sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/samba/samba4/g' package/lean/default-settings/files/zzz-default-settings	
 #sed -i '/exit 0/i\chmod 755 /etc/init.d/serverchan' package/lean/default-settings/files/zzz-default-settings	
 #sed -i '/exit 0/i\chmod 755 /usr/bin/serverchan/serverchan' package/lean/default-settings/files/zzz-default-settings	
 sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/default-settings/files/zzz-default-settings
