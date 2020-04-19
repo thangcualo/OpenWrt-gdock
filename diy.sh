f593d60feb27880be43d1c75981941a79afdc22c
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-04-19 23:55:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -23,9 +23,9 @@ sed -i '45,48d' package/lean/default-settings/files/zzz-default-settings
 #git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
 
 #改qb版本为4.2.3
-#rm -rf package/lean/qBittorrent/Makefile
-#rm -rf package/lean/qBittorrent/patches
-#cp -f ../qb423 package/lean/qBittorrent/Makefile
+rm -rf package/lean/qBittorrent/Makefile
+rm -rf package/lean/qBittorrent/patches
+cp -f ../qb423 package/lean/qBittorrent/Makefile
 
 #删除自带的插件
 #rm -rf feeds/extra/luci-app-samba4
@@ -36,7 +36,7 @@ sed -i '45,48d' package/lean/default-settings/files/zzz-default-settings
 #添加自己repo的插件的软连接
 ln -s ../../luci-theme-argon1.x ./package/
 ln -s ../../luci-app-flowoffload_ADGHome ./package/
-ln -s ../../G-DOCK/luci-app-passwall ./package/
+ln -s ../G-DOCK/luci-app-passwall ./package/
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -53,7 +53,8 @@ sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.
 #改4.19内核
 #sed -i 's/4.14/4.19/g' target/linux/ipq40xx/Makefile
 #修改zzz-default-settings的配置
-sed -i 's/samba.lua/samba4.lua/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/\"services\"/\"nas\"/g' package/lean/luci-app-samba4/luasrc/controller/samba4.lua
+#sed -i 's/samba.lua/samba4.lua/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\chmod 755 /etc/init.d/serverchan' package/lean/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\chmod 755 /usr/bin/serverchan/serverchan' package/lean/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\echo 0xDEADBEEF > /etc/config/google_fu_mode\n' package/lean/default-settings/files/zzz-default-settings
