8649b994c3b45059e0d3ad94bcff6cd53cd73d6f
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-04-19 23:10:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,26 +6,26 @@
 #   Blog: https://p3terx.com
 #=================================================
 #git clone -b dev-master https://github.com/Lienol/openwrt
-#git clone https://github.com/coolsnowwolf/lede lede
-#rm -rf package/lean
-#cp -rf lede/package/lean package/lean
-#sed -i '45,48d' package/lean/default-settings/files/zzz-default-settings
+git clone https://github.com/coolsnowwolf/lede lede
+rm -rf package/lean
+cp -rf lede/package/lean package/lean
+sed -i '45,48d' package/lean/default-settings/files/zzz-default-settings
 #添加Lienol的插件包
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git lienol1 https://github.com/a736399919/lienol-openwrt-package' feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
-awk 'BEGIN { cmd="cp -ri feeds/lienol1/* feeds/lienol/"; print "n" |cmd; }'
-rm -rf feeds/lienol1
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol1 https://github.com/a736399919/lienol-openwrt-package' feeds.conf.default
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+#awk 'BEGIN { cmd="cp -ri feeds/lienol1/* feeds/lienol/"; print "n" |cmd; }'
+#rm -rf feeds/lienol1
 #添加自定义插件
 #git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
 
 #改qb版本为4.2.3
-rm -rf package/lean/qBittorrent/Makefile
-rm -rf package/lean/qBittorrent/patches
-cp -f ../qb423 package/lean/qBittorrent/Makefile
+#rm -rf package/lean/qBittorrent/Makefile
+#rm -rf package/lean/qBittorrent/patches
+#cp -f ../qb423 package/lean/qBittorrent/Makefile
 
 #删除自带的插件
 #rm -rf feeds/extra/luci-app-samba4
@@ -36,6 +36,7 @@ cp -f ../qb423 package/lean/qBittorrent/Makefile
 #添加自己repo的插件的软连接
 ln -s ../../luci-theme-argon1.x ./package/
 ln -s ../../luci-app-flowoffload_ADGHome ./package/
+ln -s ../../G-DOCK/luci-app-passwall ./package/
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
