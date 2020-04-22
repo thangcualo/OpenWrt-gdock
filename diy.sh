9df000a238a7ae0f1a038c177def0c9e2fee9e80
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-04-22 19:30:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -46,6 +46,7 @@ sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 #修改wifi名称
 sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
