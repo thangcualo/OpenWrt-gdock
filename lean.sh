d0cdd23c8446488cf83e2302970a3b1c8ab8f74a
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-04-24 11:05:31 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -21,8 +21,10 @@ sed -i '$a src-git lienol https://github.com/a736399919/lienol-openwrt-package' 
 #git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
+
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.5.1
 rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/head-icon.jpg
+rm -rf package/luci-theme-argon1.5/htdocs/luci-static/argon/img/
 cp -rf package/luci-theme-argon-1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/
 sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
 sed -i '/background-image/d' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
