8fff023482b0b247e476dcbd541b217e54492012
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-04-25 18:50:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -36,7 +36,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.5.1
 rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/head-icon.jpg
-rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/img/
+rm -rf package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/img
 cp -rf ../luci-theme-argon-1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/
 sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
 sed -i '/background-image/d' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
