1ea8d614ff0acf58d48ec29562ba784c6a8a22df
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-05-13 21:15:53 +0800

    Update Lienol-19.07.sh

--- Lienol-19.07.sh
+++ Lienol-19.07.sh
@@ -30,8 +30,16 @@ sed -i 's/1.1.13/1.2.6/g' package/lean/rblibtorrent/Makefile
 sed -i 's/6f1250c6535730897909240ea0f4f2a81937d21a/a9968916ca82366f1c236af59aaecb9bc94ffe73/g' package/lean/rblibtorrent/Makefile
 
 #添加自己repo的插件的软连接或copy
-ln -s ../../luci-theme-argon1.x ./package/
+cp -rf ../luci-theme-argon1.x package/
 cp -rf ../G-DOCK/luci-app-passwall package
+#添加openwrt-usb-modeswitch-official
+git clone https://github.com/gzhechu/openwrt-usb-modeswitch-official.git package/openwrt-usb-modeswitch-official
+sed -i 's/2.2.0/2.6.0/g' package/openwrt-usb-modeswitch-official/Makefile
+sed -i 's/f323fe700edd6ea404c40934ddf32b22/be73dcc84025794081a1d4d4e5a75e4c/g' package/openwrt-usb-modeswitch-official/Makefile
+sed -i 's/20140529/20191128/g' package/openwrt-usb-modeswitch-official/Makefile
+sed -i 's/dff94177781298aaf0b3c2a3c3dea6b2/e8fce7eb949cbe16c61fb71bade4cc17/g' package/openwrt-usb-modeswitch-official/Makefile
+
+
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
