2fd284437754cceb272508594ba7f62338daa1ef
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-05-14 17:42:00 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -28,13 +28,13 @@ rm -rf package/luci-theme-argon1.5/htdocs/luci-static/argon/img/
 cp -rf package/luci-theme-argon-1.x/htdocs/luci-static/argon/head-icon.jpg package/luci-theme-argon-1.5.1/htdocs/luci-static/argon/
 sed -i '/class="darkMask"/a \ \ \ <div class="login-bg" style="background-color: #5e72e4"></div>' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
 sed -i '/background-image/d' package/luci-theme-argon-1.5.1/luasrc/view/themes/argon/header.htm
-#改qb版本为4.2.3
+#改qb版本为4.2.5
 rm -rf package/lean/qBittorrent/Makefile
 rm -rf package/lean/qBittorrent/patches
 cp -rf ../qb425 package/lean/qBittorrent/Makefile
 sed -i 's/1.1.13/1.2.6/g' package/lean/rblibtorrent/Makefile
 sed -i 's/6f1250c6535730897909240ea0f4f2a81937d21a/a9968916ca82366f1c236af59aaecb9bc94ffe73/g' package/lean/rblibtorrent/Makefile
-sed -i 's/R20.4.8/R18/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R20.5.9/R18/g' package/lean/default-settings/files/zzz-default-settings
 
 #添加自己repo的插件的软连接
 cp -rf ../luci-theme-argon1.x package/
