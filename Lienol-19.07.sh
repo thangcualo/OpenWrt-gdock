675cfbe7b4b54ac318f4c9138417a76a36aaa3c0
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-05-25 03:50:48 +0800

    Update Lienol-19.07.sh

--- Lienol-19.07.sh
+++ Lienol-19.07.sh
@@ -13,6 +13,7 @@ rm -rf lede/package/lean/default-settings
 rm -rf lede/package/lean/openwrt-fullconenat
 rm -rf openwrt/package/lean/
 cp -rf lede/package/lean/ openwrt/package/
+[ -e files ] && mv files openwrt/files
 cd openwrt
 #sed -i '/lienol/d' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/a736399919/lienol-openwrt-package' feeds.conf.default
@@ -33,12 +34,15 @@ sed -i 's/6f1250c6535730897909240ea0f4f2a81937d21a/a9968916ca82366f1c236af59aaec
 #添加自己repo的插件的软连接或copy
 ln -s ../../luci-theme-argon-1.x ./package/
 cp -rf ../G-DOCK/luci-app-passwall package
+
 ##添加openwrt-usb-modeswitch-official
 #git clone https://github.com/gzhechu/openwrt-usb-modeswitch-official.git package/openwrt-usb-modeswitch-official
 #sed -i 's/2.2.0/2.6.0/g' package/openwrt-usb-modeswitch-official/Makefile
 #sed -i 's/f323fe700edd6ea404c40934ddf32b22/be73dcc84025794081a1d4d4e5a75e4c/g' package/openwrt-usb-modeswitch-official/Makefile
 #sed -i 's/20140529/20191128/g' package/openwrt-usb-modeswitch-official/Makefile
 #sed -i 's/dff94177781298aaf0b3c2a3c3dea6b2/e8fce7eb949cbe16c61fb71bade4cc17/g' package/openwrt-usb-modeswitch-official/Makefile
+##添加ZTE-MF832S网卡
+#cp -rf ../ZTE-MF832S/* files/etc/
 ##添加4G网卡网络接口
 #sed -i '/exit 0/i\uci set network.4G_LTE=interface' package/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\uci set network.4G_LTE.ifname=eth1' package/default-settings/files/zzz-default-settings
@@ -48,15 +52,12 @@ cp -rf ../G-DOCK/luci-app-passwall package
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
 #修改机器名称
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
-
 #修改wifi名称
 sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
@@ -72,8 +73,5 @@ sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-
 #修改banner
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
-[ -e ../files ] && mv ../files files
 [ -e ../G-DOCK/default.config ] && mv -f ../G-DOCK/default.config .config
 [ -e ../G-DOCK/Lienol-19.07*.config ] && mv -f ../G-DOCK/Lienol-19.07*.config .config
-#cp -rf ../G-DOCK/zzz-default-settings package/default-settings/files/zzz-default-settings
-
