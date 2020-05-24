464ac23bf6062fa4194f719ad277cd3440534d39
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-05-25 03:19:16 +0800

    Update Lienol-19.07.sh

--- Lienol-19.07.sh
+++ Lienol-19.07.sh
@@ -33,13 +33,18 @@ sed -i 's/6f1250c6535730897909240ea0f4f2a81937d21a/a9968916ca82366f1c236af59aaec
 #添加自己repo的插件的软连接或copy
 ln -s ../../luci-theme-argon-1.x ./package/
 cp -rf ../G-DOCK/luci-app-passwall package
-#添加openwrt-usb-modeswitch-official
-git clone https://github.com/gzhechu/openwrt-usb-modeswitch-official.git package/openwrt-usb-modeswitch-official
-sed -i 's/2.2.0/2.6.0/g' package/openwrt-usb-modeswitch-official/Makefile
-sed -i 's/f323fe700edd6ea404c40934ddf32b22/be73dcc84025794081a1d4d4e5a75e4c/g' package/openwrt-usb-modeswitch-official/Makefile
-sed -i 's/20140529/20191128/g' package/openwrt-usb-modeswitch-official/Makefile
-sed -i 's/dff94177781298aaf0b3c2a3c3dea6b2/e8fce7eb949cbe16c61fb71bade4cc17/g' package/openwrt-usb-modeswitch-official/Makefile
-
+##添加openwrt-usb-modeswitch-official
+#git clone https://github.com/gzhechu/openwrt-usb-modeswitch-official.git package/openwrt-usb-modeswitch-official
+#sed -i 's/2.2.0/2.6.0/g' package/openwrt-usb-modeswitch-official/Makefile
+#sed -i 's/f323fe700edd6ea404c40934ddf32b22/be73dcc84025794081a1d4d4e5a75e4c/g' package/openwrt-usb-modeswitch-official/Makefile
+#sed -i 's/20140529/20191128/g' package/openwrt-usb-modeswitch-official/Makefile
+#sed -i 's/dff94177781298aaf0b3c2a3c3dea6b2/e8fce7eb949cbe16c61fb71bade4cc17/g' package/openwrt-usb-modeswitch-official/Makefile
+##添加4G网卡网络接口
+#sed -i '/exit 0/i\uci set network.4G_LTE=interface' package/default-settings/files/zzz-default-settings
+#sed -i '/exit 0/i\uci set network.4G_LTE.ifname=eth1' package/default-settings/files/zzz-default-settings
+#sed -i '/exit 0/i\uci set network.4G_LTE.proto=dhcp' package/default-settings/files/zzz-default-settings
+#sed -i '/exit 0/i\uci commit network' package/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i 's/wan wan6/wan wan6 4G_LTE/g' /etc/config/firewall\n" package/default-settings/files/zzz-default-settings
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -55,14 +60,14 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-修改插件位置
-sed -i 's/services/nas/g' package/lean/luci-app-samba4/luasrc/controller/samba4.lua
+#修改网络共享的位置
+sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/samba4.lua" package/default-settings/files/zzz-default-settings
 #修改zzz-default-settings的配置
 #添加简易网盘
-sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash\n' package/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i\ln -sv /mnt/sda1 /srv/webd/web/U盘\n' package/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i\ln -sv /mnt/mmcblk0p1/all /srv/webd/web/SD卡\n' package/default-settings/files/zzz-default-settings
-sed -i '/exit 0/i\chmod 775 /usr/bin/webd' package/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash' package/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\ln -sv /mnt/sda1 /srv/webd/web/U盘' package/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\ln -sv /mnt/mmcblk0p1/all /srv/webd/web/SD卡' package/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
 
 #修改banner
 rm -rf package/base-files/files/etc/banner
