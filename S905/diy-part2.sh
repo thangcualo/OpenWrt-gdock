0da634bf06282fbe5ff2d6811b3ae10ecb380577
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-09-22 01:28:05 +0800

    Create diy-part2.sh

---
+++ S905/diy-part2.sh
@@ -0,0 +1,8 @@
+复制files文件夹到openwrt目录
+[ -e /S905/files ] && mv /S905/files files
+#添加argon主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#添加简易网盘
+sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/lean/default-settings/files/zzz-default-settings
