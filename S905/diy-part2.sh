e4c2e4119061bdf8cb1c69c465d106159ffc59d9
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-09-23 18:35:46 +0800

    Update diy-part2.sh

--- S905/diy-part2.sh
+++ S905/diy-part2.sh
@@ -1,5 +1,5 @@
 复制files文件夹到openwrt目录
-[ -e /S905/files ] && mv /S905/files files
+[ -e ../S905/files ] && mv ../S905/files files
 #添加argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
