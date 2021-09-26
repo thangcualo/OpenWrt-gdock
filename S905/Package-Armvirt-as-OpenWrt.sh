af77df6b741f28d937a16cea5325881de7fc01bf
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-09-27 02:18:11 +0800

    Create Package-Armvirt-as-OpenWrt.sh

---
+++ S905/Package-Armvirt-as-OpenWrt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+svn co https://github.com/breakings/OpenWrt/trunk/opt/kernel/5.4.149 opt/kernel
+mv 5.4.149/* opt/kernel
+cd opt
+git clone https://github.com/unifreq/openwrt_packit
+cd openwrt_packit
+wget http://91io.cn/s/WOqVqF8/openwrt-armvirt-64-default-rootfs.tar.gz
+mv $GITHUB_WORKSPACE/opt /
+cd /opt/openwrt_packit
+./mk_s905d_n1.sh
+mv /opt $GITHUB_WORKSPACE/
