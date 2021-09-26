b9b661c493edff37caffe8ab6b5a545550dcf6de
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-09-27 07:32:54 +0800

    Update Package-Armvirt-as-OpenWrt.sh

--- S905/Package-Armvirt-as-OpenWrt.sh
+++ S905/Package-Armvirt-as-OpenWrt.sh
@@ -12,7 +12,7 @@ svn co https://github.com/breakings/OpenWrt/trunk/opt/kernel/5.4.148 /opt/kernel
 cd /opt
 git clone https://github.com/unifreq/openwrt_packit
 cd openwrt_packit
-sed -i 's/5.14.8-flippy-65+/5.4.148-flippy-64+o' make.env
+sed -i 's/5.14.8-flippy-65+/5.4.148-flippy-64+o/g' make.env
 wget http://91io.cn/s/WOqVqF8/openwrt-armvirt-64-default-rootfs.tar.gz
 sudo ./mk_s912_zyxq.sh
 sudo ./mk_s905x2_x96max.sh
