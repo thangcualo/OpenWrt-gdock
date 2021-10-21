e6db3b16b2ef6d9e6573ac0bd58e9b6906afa649
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-10-22 00:01:09 +0800

    Update Boos4721.sh

--- Boos4721.sh
+++ Boos4721.sh
@@ -7,7 +7,6 @@
 #=================================================
 #克隆源码
 git clone https://github.com/Boos4721/openwrt openwrt
-sed -i 's/5.4/5.10/g' openwrt/target/linux/ipq40xx/Makefile
 [ -e files ] && mv files openwrt/files
 cd openwrt
 ./scripts/feeds update -a
