d37ec614906737722c08a2479e8d3d1a8aef4cd8
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-11-17 18:41:46 +0800

    Update diy-part1.sh

--- S905/diy-part1.sh
+++ S905/diy-part1.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/coolsnowwolf/lede openwrt
+git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
+#git clone https://github.com/coolsnowwolf/lede openwrt
 #添加passwall
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' openwrt/feeds.conf.default
