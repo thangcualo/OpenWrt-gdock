e0facd6f41fb083fc17330f77ceb872c309e3372
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-11-18 23:26:41 +0800

    Update and rename diy-part1.sh to Lienol.sh

---
+++ S905/Lienol.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
+#添加passwall
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' openwrt/feeds.conf.default
+
+cd openwrt
+./scripts/feeds update -a
+./scripts/feeds install -a
+#移除不用软件包
+rm -rf package/lean/luci-app-dockerman
+rm -rf package/lean/luci-app-wrtbwmon
+rm -rf feeds/packages/net/smartdns
+
+#add luci-app-dockerman
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman package/luci-app-dockerman
+#添加argon主题
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#添加简易网盘
+sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash' package/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
+
+#添加自定义插件
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+[ -e ../S905/s905-Lienol.config ] && mv -f ../S905/s905-Lienol.config .config
+
