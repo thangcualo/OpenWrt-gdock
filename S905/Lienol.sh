dd4f84ca1a184da82f54b62448583d692c0046ae
a736399919 <55206982+a736399919@users.noreply.github.com>
2021-11-18 23:29:46 +0800

    Update Lienol.sh

--- S905/Lienol.sh
+++ S905/Lienol.sh
@@ -24,6 +24,7 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-1.7.2
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #添加简易网盘
+[ -e ../S905/files ] && mv ../S905/files openwrt/files
 sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash' package/default-settings/files/zzz-default-settings
 sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
 
