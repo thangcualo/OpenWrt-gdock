510a4246ac7442272760712e9814ecff42790bc0
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-15 01:22:07 +0800

    Update Google-WiFi.sh

--- Google-WiFi.sh
+++ Google-WiFi.sh
@@ -6,15 +6,17 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-git clone -b gale --single-branch https://github.com/computersforpeace/openwrt openwrt
+#git clone -b gale --single-branch https://github.com/computersforpeace/openwrt openwrt
+git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
+rm -rf openwrt/target
+svn checkout https://github.com/computersforpeace/openwrt/branches/gale/target openwrt/target
 [ -e files ] && mv files openwrt/files
 cd openwrt
 #添加passwall
-#sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
 
 #添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
@@ -29,6 +31,9 @@ sed -i 's/OpenWrt/Google/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+#更改主机型号，支持中文。 
+sed -i "s/Google WiFi (Gale)/谷歌路由器/g" target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-wifi.dts
+
 #修改zzz-default-settings的配置
 #修改网络共享的位置
 #sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/samba4.lua" package/default-settings/files/zzz-default-settings
