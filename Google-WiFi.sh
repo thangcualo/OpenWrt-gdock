196849ce8436d060803c273d095f891d6ea22a32
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-16 13:22:15 +0800

    Update Google-WiFi.sh

--- Google-WiFi.sh
+++ Google-WiFi.sh
@@ -8,8 +8,8 @@
 #克隆源码
 #git clone -b gale --single-branch https://github.com/computersforpeace/openwrt openwrt
 git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
-rm -rf openwrt/target
-svn checkout https://github.com/computersforpeace/openwrt/branches/gale/target openwrt/target
+rm -rf openwrt/target/linux/ipq40xx
+svn checkout https://github.com/computersforpeace/openwrt/branches/gale/target/linux/ipq40xx openwrt/target/linux/ipq40xx
 [ -e files ] && mv files openwrt/files
 cd openwrt
 #添加passwall
@@ -20,6 +20,11 @@ sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' 
 
 #添加主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
+
+#添加自定义插件
+git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -36,8 +41,8 @@ sed -i "s/Google WiFi (Gale)/谷歌路由器/g" target/linux/ipq40xx/files/arch/
 
 #修改zzz-default-settings的配置
 #修改网络共享的位置
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/samba4.lua" package/default-settings/files/zzz-default-settings
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-samba4.json" package/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/ksmbd.lua" package/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-ksmbd.json" package/default-settings/files/zzz-default-settings
 
 #修改aria2的位置
 #sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/aria2.lua" package/default-settings/files/zzz-default-settings
@@ -48,8 +53,32 @@ sed -i "s/Google WiFi (Gale)/谷歌路由器/g" target/linux/ipq40xx/files/arch/
 #sed -i '/exit 0/i\ln -sv /mnt/mmcblk0p1/all /srv/webd/web/SD卡' package/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
 
-#修改banner
-#mv -f ../G-DOCK/Google.default .config
-mv -f ../G-DOCK/Google_20220215.config .config
+##
 wget -P scripts https://raw.githubusercontent.com/computersforpeace/openwrt/gale/scripts/gen_image_vboot.sh
 chmod +x scripts/gen_image_vboot.sh
+
+rm -rf package/base-files/files/lib/upgrade/common.sh
+wget -P package/base-files/files/lib/upgrade https://raw.githubusercontent.com/computersforpeace/openwrt/gale/package/base-files/files/lib/upgrade/common.sh
+chmod +x package/base-files/files/lib/upgrade/common.sh
+
+rm -rf include/image-commands.mk
+wget -P include https://raw.githubusercontent.com/computersforpeace/openwrt/gale/include/image-commands.mk
+
+rm -rf package/base-files/Makefile
+wget -P package/base-files https://raw.githubusercontent.com/computersforpeace/openwrt/gale/package/base-files/Makefile
+
+rm -rf package/base-files/files/lib/upgrade/emmc.sh
+wget -P package/base-files/files/lib/upgrade https://raw.githubusercontent.com/computersforpeace/openwrt/gale/package/base-files/files/lib/upgrade/emmc.sh
+chmod +x scripts/gen_image_vboot.sh package/base-files/files/lib/upgrade/emmc.sh
+
+rm -rf scripts/target-metadata.pl
+wget -P scripts https://raw.githubusercontent.com/computersforpeace/openwrt/gale/scripts/target-metadata.pl
+chmod +x scripts/target-metadata.pl
+
+rm -rf target/Config.in
+wget -P target https://raw.githubusercontent.com/computersforpeace/openwrt/gale/target/Config.in
+##
+
+#修改banner
+#mv -f ../G-DOCK/Google.default .config
+mv -f ../G-DOCK/Google_Lienol*.config .config
