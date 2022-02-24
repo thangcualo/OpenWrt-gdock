62b4565c85fe6c4dd56a83f5ba02360b4406c2de
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-24 13:41:06 +0800

    Update Google-WiFi.sh

--- Google-WiFi.sh
+++ Google-WiFi.sh
@@ -22,11 +22,13 @@ svn checkout https://github.com/immortalwrt/luci/trunk/themes/luci-theme-argon p
 svn checkout https://github.com/immortalwrt/luci/trunk/themes/luci-theme-material package/luci-theme-material
 #git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.2.9
 
-#添加自定义插件
+
 svn checkout https://github.com/Lienol/openwrt/branches/main/package/default-settings package/default-settings
+#添加自定义插件
+git clone https://github.com/tcsr200722/luci-app-cpufreq package/luci-app-cpufreq
 svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-webd package/luci-app-webd
 svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/webd package/webd
-svn checkout https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq package/luci-app-cpufreq
+sed -i '$a chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
 git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
@@ -46,18 +48,30 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 sed -i "s/Google WiFi (Gale)/谷歌路由器/g" target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-wifi.dts
 
 #修改zzz-default-settings的配置
+删除包含"exit 0"的行
+sed -i '/exit 0/d' package/default-settings/files/zzz-default-settings
+#添加LingMaxDNS
+chmod +x files/etc/LingMaxDns_linux_arm
+sed -i '$a sed -i '\''$a iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 8287'\'' /etc/firewall.user' package/default-settings/files/zzz-default-settings
+sed -i '$a sed -i '\''/exit 0/i\/etc/LingMaxDns_linux_arm &'\'' /etc/rc.local' package/default-settings/files/zzz-default-settings
+
 #修改网络共享的位置
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/samba4.lua" package/default-settings/files/zzz-default-settings
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-samba4.json" package/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/ksmbd.lua" package/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-ksmbd.json" package/default-settings/files/zzz-default-settings
+sed -i '$a sed -i '\''s/services/nas/g'\'' /usr/lib/lua/luci/controller/samba4.lua' package/default-settings/files/zzz-default-settings
+sed -i '$a\sed -i '\''s/services/nas/g'\'' /usr/share/luci/menu.d/luci-app-samba4.json' package/default-settings/files/zzz-default-settings
+sed -i '$a\sed -i '\''s/services/nas/g'\'' /usr/lib/lua/luci/controller/ksmbd.lua' package/default-settings/files/zzz-default-settings
+sed -i '$a\sed -i '\''s/services/nas/g'\'' /usr/share/luci/menu.d/luci-app-ksmbd.json' package/default-settings/files/zzz-default-settings
 
-
+#修改aria2的位置
+#sed -i '$a sed -i '\''s/services/nas/g'\'' /usr/lib/lua/luci/controller/aria2.lua' package/default-settings/files/zzz-default-settings
+#修改oaf的位置
+#sed -i '$a sed -i '\''s/network/control/g'\'' /usr/lib/lua/luci/controller/appfilter.lua' package/default-settings/files/zzz-default-settings
 #添加简易网盘
 #sed -i '/exit 0/i\mkdir -pv /srv/webd/web/.Trash' package/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\ln -sv /mnt/sda1 /srv/webd/web/U盘' package/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\ln -sv /mnt/mmcblk0p1/all /srv/webd/web/SD卡' package/default-settings/files/zzz-default-settings
 #sed -i '/exit 0/i\chmod 775 /usr/bin/webd\n' package/default-settings/files/zzz-default-settings
+#添加包含"exit 0"的行
+sed -i '$a\exit 0' package/default-settings/files/zzz-default-settings
 
 #修改banner
 #mv -f ../G-DOCK/google_wifi_lienol.default .config
