8950b3ca044fb02bf4db76cb18a1f1e1137b36e6
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-02-24 14:03:31 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -64,7 +64,7 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 #修改zzz-default-settings的配置
-删除包含"exit 0"的行
+#删除包含"exit 0"的行
 sed -i '/exit 0/d' package/default-settings/files/zzz-default-settings
 #添加LingMaxDNS
 chmod +x files/etc/LingMaxDns_linux_arm
