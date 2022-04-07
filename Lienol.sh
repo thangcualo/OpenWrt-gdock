884dd367f839080e7f4a7fa7f2d96cb0a973dbcd
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-04-08 02:55:59 +0800

    Update Lienol.sh

--- Lienol.sh
+++ Lienol.sh
@@ -6,16 +6,16 @@
 #   Blog: https://p3terx.com
 #=================================================
 #克隆源码
-git clone -b 21.02 --single-branch https://github.com/Lienol/openwrt openwrt
+git clone -b 22.03 --single-branch https://github.com/Lienol/openwrt openwrt
 #git clone -b main --single-branch https://github.com/Lienol/openwrt openwrt
 [ -e files ] && mv files openwrt/files
 cd openwrt
 #添加passwall
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git xiaorouji1 https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-passwall feeds/xiaorouji/luci-app-passwall
 #添加自定义插件
 git clone https://github.com/tcsr200722/luci-app-cpufreq package/luci-app-cpufreq
 svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-webd package/luci-app-webd
@@ -68,8 +68,9 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 sed -i '/exit 0/d' package/default-settings/files/zzz-default-settings
 #添加LingMaxDNS
 chmod +x files/etc/LingMaxDns_linux_arm
+chmod +x files/etc/init.d/LingMaxDns
+sed -i '$a ln -s /etc/init.d/LingMaxDns /etc/rc.d/S999LingMaxDns' package/default-settings/files/zzz-default-settings
 sed -i '$a sed -i '\''$a iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 8287'\'' /etc/firewall.user' package/default-settings/files/zzz-default-settings
-sed -i '$a sed -i '\''/exit 0/i\/etc/LingMaxDns_linux_arm >/dev/null 2>&1 &'\'' /etc/rc.local' package/default-settings/files/zzz-default-settings
 
 #修改网络共享的位置
 sed -i '$a sed -i '\''s/services/nas/g'\'' /usr/lib/lua/luci/controller/samba4.lua' package/default-settings/files/zzz-default-settings
@@ -93,5 +94,5 @@ rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 #mv -f ../G-DOCK/Lienol.default .config
 mv -f ../G-DOCK/Lienol_*.config .config
-cp -f ../G-DOCK/999-ipq40xx-unlock-cpu-frequency.patch target/linux/ipq40xx/patches-5.4/999-ipq40xx-unlock-cpu-frequency.patch
+#cp -f ../G-DOCK/999-ipq40xx-unlock-cpu-frequency.patch target/linux/ipq40xx/patches-5.4/999-ipq40xx-unlock-cpu-frequency.patch
 
