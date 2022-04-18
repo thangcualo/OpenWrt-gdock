cdcf130353b9340b7f324c67b7b888ac45d16790
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-04-18 17:46:59 +0800

    Update Package-Armvirt-as-OpenWrt.sh

--- S905/Package-Armvirt-as-OpenWrt.sh
+++ S905/Package-Armvirt-as-OpenWrt.sh
@@ -13,10 +13,10 @@ cd /opt
 git clone https://github.com/unifreq/openwrt_packit
 cd openwrt_packit
 sed -i 's/5.17.3-flippy-71+/5.17.3-kissyouhunter/g' make.env
-sed -i 's/R22.4.1/OpenWrt 22.03/g' make.env
+sed -i 's/R22.4.1/OpenWrt_22.03/g' make.env
 sed -i 's/ENABLE_WIFI_K510=1/ENABLE_WIFI_K510=0/g' make.env
 sed -i 's/SW_FLOWOFFLOAD=1/SW_FLOWOFFLOAD=0/g' make.env
 sed -i 's/SFE_FLOW=0/SFE_FLOW=1/g' make.env
 wget -O  "openwrt-armvirt-64-default-rootfs.tar.gz" https://tmp-reach.vx-cdn.com/file-625d2f772979e/openwrt-armvirt-64-default-rootfs.tar.gz
 sudo ./mk_s905_mxqpro+.sh
-sudo mv /opt $GITHUB_WORKSPACE/
+sudo cp -rf /opt $GITHUB_WORKSPACE/
