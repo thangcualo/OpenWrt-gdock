bbb74d53d862b93d4093e8377c1eb2c05ab83edc
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-09-05 04:28:18 +0800

    Update immortalwrt.sh

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -29,7 +29,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/GDOCK/g' package/base-files/files/bin/config_generate
 #修改wifi名称
 sed -i 's/OpenWrt/GDOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#默认打开WiFi
+#默认打开WiFi了
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
