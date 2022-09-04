6eef809cd789d6086476a45e6e500882dd6091df
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-09-05 04:58:17 +0800

    Update immortalwrt.sh

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -20,8 +20,8 @@ git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
 
 svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-webd package/luci-app-webd
-svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/webd package/webd
-sed -i '$a chmod 775 /usr/bin/webd\n' package/emortal/default-settings/files/99-default-settings
+#svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/webd package/webd
+#sed -i '$a chmod 775 /usr/bin/webd\n' package/emortal/default-settings/files/99-default-settings
 
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
@@ -29,7 +29,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/GDOCK/g' package/base-files/files/bin/config_generate
 #修改wifi名称
 sed -i 's/OpenWrt/GDOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#默认打开WiFi了
+#默认打开WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -60,5 +60,5 @@ sed -i '$a\exit 0' package/emortal/default-settings/files/99-default-settings
 
 rm -rf package/base-files/files/etc/banner
 cp -rf ../banner package/base-files/files/etc/
-#mv -f ../G-DOCK/immortalwrt.config .config
+#mv -f ../G-DOCK/immortalwrt.default .config
 mv -f ../G-DOCK/immortalwrt-*.config .config
