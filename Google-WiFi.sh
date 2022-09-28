e891166c589dd50d44e7d52389437a35924b80a3
a736399919 <55206982+a736399919@users.noreply.github.com>
2022-09-29 00:42:47 +0800

    Update Google-WiFi.sh

--- Google-WiFi.sh
+++ Google-WiFi.sh
@@ -55,5 +55,4 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #修改时区
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-mv -f ../G-DOCK/default.config .config
-mv -f ../G-DOCK/x-wrt-google.config .config
+mv -f ../Google-Wifi/x-wrt-google.config .config
